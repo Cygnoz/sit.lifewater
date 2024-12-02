297942fe3dce690f829fd91c8a84f5382495230d
SanalCygnoz <sanalk2003@gmail.com>
2024-12-02 17:48:49 +0530

    button

--- src/commoncomponents/Buttons/Button.tsx
+++ src/commoncomponents/Buttons/Button.tsx
@@ -1,6 +1,6 @@
 // Path: components/Button.tsx
 import { cva } from "class-variance-authority";
- 
+
 type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
   variant?: "primary" | "secondary" | "tertiary" | "fourthiary";
   size?: "sm" | "md" | "lg" | "xl";
@@ -9,21 +9,21 @@ type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
   onSubmit?: (event: React.FormEvent<HTMLButtonElement>) => void; // Added onSubmit event type
 };
 
- 
+
 const buttonVariants = cva("flex text-center items-center", {
   variants: {
     variant: {
       primary:
-        "bg-primary_main hover:bg-primary_hover active:bg-primary_active disabled:bg-primary_disabled rounded-md gap-2 border border-gray-500 text-white",
+        "bg-[#820000] rounded-md gap-2 border border-gray-500 text-white",
       secondary:
         "bg-secondary_main hover:bg-secondary_hover active:bg-secondary_active disabled:bg-secondary_disabled rounded-md gap-2 border border-outlineButton text-outlineButton",
       tertiary:
         "bg-tertiary_main hover:bg-tertiary_hover active:bg-tertiary_active disabled:bg-tertiary_disabled rounded-md gap-2 border border-outlineButton text-outlineButton",
       fourthiary:
-        "bg-fourthiary_main hover:bg-fourthiary_hover active:bg-fourthiary_active disabled:bg-fourthiary_disabled rounded-md gap-2 border border-gray-500 text-white",
+        "bg-[#F6F6F6] rounded-md gap-2 border border-[#820000] text-[fourthiary]",
     },
     size: {
-      sm: "px-[0.625rem] py-2 rounded-[8px]",
+      sm: " py-1 px-[1rem] rounded-[8px]",
       md: "px-[0.625rem] py-3 rounded-lg",
       lg: "px-4 py-3 rounded-lg ",
       xl: "px-4 py-3 rounded-lg",
@@ -31,32 +31,32 @@ const buttonVariants = cva("flex text-center items-center", {
   },
   defaultVariants: {
     variant: "primary",
-    size: "md",
+    size: "sm",
   },
 });
- 
+
 export default function Button({
   variant = "primary",
-  size = "md",
+  size = "sm",
   className = "p-4",
   type = "button",
   onSubmit, // Added onSubmit prop
   ...props
 }: ButtonProps) {
   const combinedClassName = `${buttonVariants({ variant, size })} ${className}`;
- 
+
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     if (type === "submit" && onSubmit) {
       onSubmit(event as any); // Cast to `any` to simulate `onSubmit` behavior
     }
- 
+
     if (props.onClick) {
       props.onClick(event);
     }
   };
- 
+
   return (
-<button
+    <button
       type={type}
       {...props}
       className={combinedClassName}
