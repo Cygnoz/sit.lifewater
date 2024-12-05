c6978ae95fb9983285e60bcba1e40fa184958afc
SanalCygnoz <sanalk2003@gmail.com>
2024-12-05 17:48:42 +0530

    login

---
+++ src/services/ApiEndpoint.tsx
@@ -0,0 +1,4 @@
+export const endpoints = {
+    //lofin
+    LOGIN:`staff/login`
+}
---
+++ src/services/AxiosInstance.tsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const BASE_URLS: Record<number, string> = {
+  4000: import.meta.env.VITE_REACT_APP_BACKEND,
+  4001: import.meta.env.VITE_REACT_APP_STOCK,
+  4002: import.meta.env.VITE_REACT_APP_ORDER,
+};
+
+ console.log(import.meta.env.VITE_REACT_APP_BACKEND,"api");
+
+const createInstance = (
+  port: number,
+  contentType: string,
+  useAuth: boolean
+) => {
+  const baseURL = BASE_URLS[port];
+  let headers: Record<string, string> = {
+    "Content-Type": contentType,
+    Accept: "application/json",
+  };
+  
+
+  if (useAuth) {
+  const authToken = localStorage.getItem("authToken");
+  if (authToken) {
+    headers = { ...headers, Authorization: `Bearer ${authToken}` };
+  } else {
+    console.warn("No auth token found in local storage.");
+  }
+}
+
+// console.log(headers,"headers");
+
+  return axios.create({
+    baseURL,
+    headers,
+  });
+};
+
+const baseInstance = (port: number) =>
+  createInstance(port, "application/json", false);
+
+const authInstance = (port: number) =>
+  createInstance(port, "application/json", true);
+
+const MauthInstance = (port: number) =>
+  createInstance(port, "multipart/form-data", true);
+
+export default { baseInstance, authInstance, MauthInstance };
