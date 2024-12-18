a398138512f5502985129309e8be58ca64d7e99d
abhinuak1 <abhinu.raghunathan@cygnonex.com>
2024-12-18 18:17:43 +0530

    stock module in salesman

--- src/pages/Stock.tsx
+++ src/pages/Stock.tsx
@@ -1,108 +1,170 @@
-import rupee from '../assets/images/receipt-indian-rupee.svg';
-import frame from '../assets/images/Frame 629279.svg'
-import damage from '../assets/images/Frame 629278.svg'
-import fram from '../assets/images/rupeeframe.svg'
+
+import frame from "../assets/images/Frame 629279.svg";
+import damage from "../assets/images/Frame 629278.svg";
+import fram from "../assets/images/rupeeframe.svg";
+import { useEffect, useState } from "react";
 
 const Stock: React.FC = () => {
-    return (
-        <div>
+  const [stockData, setStockData] = useState<
+    { itemName: string; quantity: number }[]
+  >([]);
 
-            <div className=" p-3 flex flex-col items-center justify-center bg-[#F5F6FA] pb-52">
-                {/* Main Container */}
-                <div className="w-full max-w-lg  p-4">
+  // Fetch stock data from local storage
+  useEffect(() => {
+    const storedData = localStorage.getItem("activeRoute");
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+      setStockData(parsedData.stock || []); // Set the stock array
+    }
+  }, []);
 
-                    {/* Top Section: Stock balance, Damage Stock, Internal Order */}
-                    <div className="grid grid-cols-3 gap-4 mb-6">
-                        {/* Stock balance */}
-                        <div className="bg-[#FFFFFF] shadow p-3 rounded-lg  flex flex-col ">
-                            <span className=" rounded-full">
-                                <img src={frame} alt="Icon" className="w-9 h-9" />
-                            </span>
-                            <span className="text-3xl font-bold text-[18px] text-left text-[#820000]">38</span>
-                           
-                            <span  className="text-[11px] font-bold leading-[13.31px] text-[#787A7D] text-left">Stock In Hand</span>
-                        </div>
-                        {/* Damage Stock */}
-                        <div className="bg-[#FFFFFF] shadow p-3 rounded-lg flex flex-col ">
-                            <span className="rounded-full">
-                                <img src={damage} alt="Icon" className="w-9 h-9" />
-                            </span>
-                            <span className="text-3xl text-[18px] font-bold text-left text-[#820000]">00</span>
-                            <span className="text-[11px] font-bold leading-[13.31px] text-left text-[#787A7D] ">Damage Stock</span>
-                        </div>
-                        {/* Internal Order */}
-                        <div className="bg-[#FFFFFF] shadow p-3 rounded-lg flex flex-col ">
-                            <span className="rounded-full">
-                                <img src={fram} alt="Icon" className="w-9 h-9" />
-                            </span>
-                            <span className="text-3xl text-[18px] font-bold text-left text-[#820000]">00</span>
-                            <span className="text-[11px] font-bold leading-[13.31px] text-left text-[#787A7D] ">Internal Order</span>
-                        </div>
-                    </div>
+  return (
+    <div className="bg-[#F5F6FA]  min-h-screen">
+      <div className=" p-3 flex flex-col items-center justify-center ">
+        {/* Main Container */}
+        <div className="w-full max-w-lg  p-4">
+          {/* Top Section: Stock balance, Damage Stock, Internal Order */}
+          <div className="grid grid-cols-3 gap-4 mb-6">
+            {/* Stock balance */}
+            <div className="bg-[#FFFFFF] shadow p-3 rounded-lg  flex flex-col ">
+              <span className=" rounded-full">
+                <img src={frame} alt="Icon" className="w-9 h-9 mb-1" />
+              </span>
+              <span className="text-3xl  font-bold text-left text-[#820000]">
+                {stockData.length}
+              </span>
 
-                    {/* Lower Section: Empty Stock, Advance Stock, Started Stock */}
-                    <div className="space-y-4 bg-[#FFFFFF] rounded shadow p-5" >
-                        {/* Empty Stock */}
-                        <div className="bg-[#F5F6FA] p-2  rounded-lg flex items-center">
-                            <div className=" w-10 h-10 flex justify-center items-center rounded-full mr-4">
-                                {/* Icon placeholder */}
+              <span className="text-[11px] font-bold leading-[13.31px] text-[#787A7D] text-left">
+                Stock In Hand
+              </span>
+            </div>
+            {/* Damage Stock */}
+            <div className="bg-[#FFFFFF] shadow p-3 rounded-lg flex flex-col ">
+              <span className="rounded-full">
+                <img src={damage} alt="Icon" className="w-9 h-9" />
+              </span>
+              <span className="text-3xl  font-bold text-left text-[#820000]">
+                -
+              </span>
+              <span className="text-[11px] font-bold leading-[13.31px] text-left text-[#787A7D] ">
+                Damage Stock
+              </span>
+            </div>
+            {/* Internal Order */}
+            <div className="bg-[#FFFFFF] shadow p-3 rounded-lg flex flex-col ">
+              <span className="rounded-full">
+                <img src={fram} alt="Icon" className="w-9 h-9" />
+              </span>
+              <span className="text-3xl  font-bold text-left text-[#820000]">
+                -
+              </span>
+              <span className="text-[11px] font-bold leading-[13.31px] text-left text-[#787A7D] ">
+                Internal Order
+              </span>
+            </div>
+          </div>
 
-                                <span className="bg-[#E3E6D5] p-2 rounded-full">
-                                    <img src={rupee} alt="Icon" className="w-6 h-6" />
-                                </span>
-                            </div>
-                            <div className='text-left'>
-                                <span className="text-[18px] font-bold text-left text-[#820000]">0</span>
-                                <span className="block text-[14px] font-bold leading-[16.94px] text-[#787A7D]">Empty Stock</span>
+          {/* Lower Section: Empty Stock, Advance Stock, Started Stock */}
+          {/* <div className="space-y-4 bg-[#FFFFFF] rounded shadow p-5">
+           
+            <div className="bg-[#F5F6FA] p-2  rounded-lg flex items-center">
+              <div className=" w-10 h-10 flex justify-center items-center rounded-full mr-4">
+                
 
-                            </div>
-                        </div>
-
-                        {/* Advance Stock */}
-                        <div className="bg-[#F5F6FA] p-2 rounded-lg flex items-center">
-                            <div className="w-10 h-10 flex justify-center items-center rounded-full mr-4">
-                                {/* Icon placeholder */}
-                                <span className="bg-[#E3E6D5] p-2 rounded-full">
-                                    <img src={rupee} alt="Icon" className="w-6 h-6" />
-                                </span>
-                            </div>
-                            <div className='text-left'>
-                                <span className="text-[18px] font-bold text-[#820000]">38</span>
-                                <span className="block text-[14px] font-bold leading-[16.94px] text-center text-[#787A7D]">Advance Stock</span>
-
-                            </div>
-                        </div>
-
-                        {/* Started Stock */}
-                        <div className="bg-[#F5F6FA] p-2 rounded-lg flex items-center">
-                            <div className="bg-gray-300 w-10 h-10 flex justify-center items-center rounded-full mr-4">
-                                {/* Icon placeholder */}
-                                <span className="bg-[#E3E6D5] p-2 rounded-full">
-                                    <img src={rupee} alt="Icon" className="w-6 h-6" />
-                                </span>
-                            </div>
-                            <div className='text-left'>
-                                <span className="text-[18px] text-[#820000] font-bold ">38</span>
-                                <span className="block text-[14px] font-bold leading-[16.94px] text-center text-[#787A7D]">Started Stock</span>
-
-                            </div>
-
-                            
-                        </div>
-                    </div>
-
-                    <div className="mt-4">
-        <div className="bg-white p-4 rounded-lg shadow text-center">
-          <span className='text-[#787A7D]'>Others</span>
-        </div>
-      </div>
-
-
-                </div>
+                <span className="bg-[#E3E6D5] p-2 rounded-full">
+                  <img src={rupee} alt="Icon" className="w-6 h-6" />
+                </span>
+              </div>
+              <div className="text-left">
+                <span className="text-[18px] font-bold text-left text-[#820000]">
+                  0
+                </span>
+                <span className="block text-[14px] font-bold leading-[16.94px] text-[#787A7D]">
+                  Empty Stock
+                </span>
+              </div>
             </div>
 
+            <div className="bg-[#F5F6FA] p-2 rounded-lg flex items-center">
+              <div className="w-10 h-10 flex justify-center items-center rounded-full mr-4">
+               
+                <span className="bg-[#E3E6D5] p-2 rounded-full">
+                  <img src={rupee} alt="Icon" className="w-6 h-6" />
+                </span>
+              </div>
+              <div className="text-left">
+                <span className="text-[18px] font-bold text-[#820000]">38</span>
+                <span className="block text-[14px] font-bold leading-[16.94px] text-center text-[#787A7D]">
+                  Advance Stock
+                </span>
+              </div>
+            </div>
+
+        
+            <div className="bg-[#F5F6FA] p-2 rounded-lg flex items-center">
+              <div className="bg-gray-300 w-10 h-10 flex justify-center items-center rounded-full mr-4">
+               
+                <span className="bg-[#E3E6D5] p-2 rounded-full">
+                  <img src={rupee} alt="Icon" className="w-6 h-6" />
+                </span>
+              </div>
+              <div className="text-left">
+                <span className="text-[18px] text-[#820000] font-bold ">
+                  38
+                </span>
+                <span className="block text-[14px] font-bold leading-[16.94px] text-center text-[#787A7D]">
+                  Started Stock
+                </span>
+              </div>
+            </div>
+          </div> */}
+          <div className="overflow-x-auto mt-5">
+            <table className="min-w-full table-auto border-collapse border border-gray-300">
+              <thead>
+                <tr className="bg-[#fdf8f0]">
+                  <th className="border border-gray-300 px-4 py-2 text-left text-gray-600">
+                    Sl.No
+                  </th>
+                  <th className="border border-gray-300 px-4 py-2 text-left text-gray-600">
+                    Item Name
+                  </th>
+                  <th className="border border-gray-300 px-4 py-2 text-left text-gray-600">
+                    Quantity
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {stockData.length > 0 ? (
+                  stockData.map((item, index) => (
+                    <tr key={index} className="hover:bg-gray-100">
+                      <td className="border border-gray-300 px-4 py-2 text-center">
+                        {index + 1}
+                      </td>
+                      <td className="border border-gray-300 px-4 py-2">
+                        {item.itemName}
+                      </td>
+                      <td className="border border-gray-300 px-4 py-2">
+                        {item.quantity}
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td
+                      className="border border-gray-300 px-4 py-2 text-center"
+                      colSpan={3}
+                    >
+                      No Stock available
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Stock;
--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const fields: string[] = [
   "Opening",
@@ -16,32 +16,39 @@ const fields: string[] = [
 
 const Summary: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-100 pt-4">
-      <div className="bg-white p-6 rounded-lg  shadow-lg w-96">
-        <div className="flex items-center justify-between mb-6"></div>
-        
-        {/* Summary Form */}
-        <form className="space-y-4">
-          {fields.map((field, index) => (
-            <div key={index} className="flex justify-between items-center">
-              <label className="text-gray-700 text-sm font-semibold">{field}</label>
-              <input
-                type="number"
-                value="20"
-                readOnly
-                className="w-24 px-3 py-1 bg-gray-300 border rounded-lg text-center text-gray-700"
-              />
-            </div>
-          ))}
-          <button
-            type="submit"
-            className="w-full bg-red-800 text-white py-2 rounded-lg hover:bg-red-700 transition duration-300"
-          >
-            Submit
-          </button>
-        </form>
-      </div>
-    </div>
+   <div> <div className="min-h-screen bg-gray-100 flex  justify-center p-2">
+   <div className="bg-white p-6 rounded-lg shadow-lg w-full ">
+     <h2 className="text-lg font-bold text-gray-800 mb-6 text-center">Summary</h2>
+
+     {/* Summary Form */}
+     <form className="space-y-4">
+       {fields.map((field, index) => (
+         <div
+           key={index}
+           className="flex justify-between items-center space-x-2"
+         >
+           <label
+             className="text-gray-700 text-sm font-semibold flex-shrink-0 w-1/2"
+           >
+             {field}
+           </label>
+           <input
+             type="number"
+             value="20"
+             readOnly
+             className="flex-grow px-3 py-1 bg-gray-100 border border-gray-300 rounded-lg text-center text-gray-700"
+           />
+         </div>
+       ))}
+       <button
+         type="submit"
+         className="w-full bg-red-800 text-white py-2 rounded-lg hover:bg-red-700 transition duration-300"
+       >
+         Submit
+       </button>
+     </form>
+   </div>
+ </div></div>
   );
 };
 
