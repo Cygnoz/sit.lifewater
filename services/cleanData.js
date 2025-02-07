14576d2d12549eef31ff3bae2b8f231151addbf4
arjunvm2 <148052201+arjunvm1@users.noreply.github.com>
2025-02-07 12:44:13 +0530

    feat: add data cleaning utility to remove null and undefined values

---
+++ services/cleanData.js
@@ -0,0 +1,9 @@
+function cleanData(data) {
+  const cleanData = (value) => (value === null || value === undefined || value === "" ? undefined : value);
+  return Object.keys(data).reduce((acc, key) => {
+    acc[key] = cleanData(data[key]);
+    return acc;
+  }, {});
+}
+
+module.exports = { cleanData };
